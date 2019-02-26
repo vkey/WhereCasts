a5f8cde52da062e0184e0e54c246684937d34531
Kris Bernardic <krisdb@gmail.com>
2019-02-25 19:16:47 -0500

    bold unread count

--- wear/src/main/java/com/krisdb/wearcasts/PodcastsAdapter.java
+++ wear/src/main/java/com/krisdb/wearcasts/PodcastsAdapter.java
@@ -3,10 +3,13 @@ package com.krisdb.wearcasts;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.wear.widget.WearableRecyclerView;
+import android.text.SpannableString;
+import android.text.style.StyleSpan;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -112,7 +115,9 @@ public class  PodcastsAdapter extends WearableRecyclerView.Adapter<PodcastsAdapt
             viewHolder.title.setText(podcast.getChannel().getTitle());
 
         if (podcast.getNewCount() > 0) {
-            viewHolder.count.setText(String.valueOf(podcast.getNewCount()));
+            final SpannableString count = new SpannableString(String.valueOf(podcast.getNewCount()));
+            count.setSpan(new StyleSpan(Typeface.BOLD), 0, count.length(), 0);
+            viewHolder.count.setText(count);
             viewHolder.count.setVisibility(View.VISIBLE);
         } else
             viewHolder.count.setVisibility(View.GONE);
