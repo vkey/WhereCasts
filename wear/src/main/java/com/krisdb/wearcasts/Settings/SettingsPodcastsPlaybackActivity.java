88f48529d5ba9faa511d966f74e97422aabdcacd
Kris Bernardic <krisdb@gmail.com>
2019-04-11 17:11:02 -0400

    updates

--- wear/src/main/java/com/krisdb/wearcasts/Settings/SettingsPodcastsPlaybackActivity.java
+++ wear/src/main/java/com/krisdb/wearcasts/Settings/SettingsPodcastsPlaybackActivity.java
@@ -19,6 +19,6 @@ public class SettingsPodcastsPlaybackActivity extends BasePreferenceActivity {
         ft.commit();
 
         //final android.database.sqlite.SQLiteDatabase sdb2 = com.krisdb.wearcasts.Databases.DatabaseHelper.select(this);
-        //sdb2.execSQL("DELETE FROM [tbl_podcast_episodes] WHERE [id] IN (SELECT [id] FROM [tbl_podcast_episodes] WHERE [pid] = 3 ORDER BY [pubdate] DESC LIMIT 1)");
+        //sdb2.execSQL("DELETE FROM [tbl_podcast_episodes] WHERE [id] IN (SELECT [id] FROM [tbl_podcast_episodes] ORDER BY [pubdate] DESC LIMIT 1)");
     }
 }
