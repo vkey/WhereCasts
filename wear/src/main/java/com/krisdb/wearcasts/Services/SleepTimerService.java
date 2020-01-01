b6dc010415b3934cc1f77f4383558506ee02a7ea
Kris Bernardic <krisdb@gmail.com>
2019-12-31 21:23:25 -0500

    sleep timer fix

--- wear/src/main/java/com/krisdb/wearcasts/Services/SleepTimerService.java
+++ wear/src/main/java/com/krisdb/wearcasts/Services/SleepTimerService.java
@@ -5,16 +5,19 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.RemoteException;
 import android.preference.PreferenceManager;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.session.MediaControllerCompat;
 
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
-import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
 import com.krisdb.wearcasts.Activities.MainActivity;
 import com.krisdb.wearcasts.R;
@@ -24,6 +27,9 @@ import java.lang.ref.WeakReference;
 public class SleepTimerService extends IntentService {
 
     private CountDownTimer timer;
+    private MediaBrowserCompat mMediaBrowser;
+    private Context mContext;
+
 
     public SleepTimerService() {
         super("SleepTimerService");
@@ -36,9 +42,9 @@ public class SleepTimerService extends IntentService {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
 
-        final Context ctx = new WeakReference<>(this).get();
+        mContext = new WeakReference<>(this).get();
 
-        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(mContext);
         final int timerMinutes = Integer.valueOf(prefs.getString("pref_sleep_timer", "0"));
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
@@ -49,10 +55,10 @@ public class SleepTimerService extends IntentService {
             final NotificationChannel channel = new NotificationChannel(channelID, getString(R.string.notification_channel_sleep_timer), NotificationManager.IMPORTANCE_DEFAULT);
             notificationManager.createNotificationChannel(channel);
 
-            final Intent notificationIntent = new Intent(ctx, MainActivity.class);
-            PendingIntent pendingIntent = PendingIntent.getActivity(ctx, 0, notificationIntent, 0);
+            final Intent notificationIntent = new Intent(mContext, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(mContext, 0, notificationIntent, 0);
 
-            final Notification notification = new NotificationCompat.Builder(ctx, channelID)
+            final Notification notification = new NotificationCompat.Builder(mContext, channelID)
                     .setContentTitle(getString(R.string.notification_channel_sleep_timer))
                     .setSmallIcon(R.drawable.ic_notification)
                     .setContentIntent(pendingIntent)
@@ -63,16 +69,23 @@ public class SleepTimerService extends IntentService {
 
         timer = new CountDownTimer(timerMinutes * 60000, 1000) {
 
-            public void onTick(long millisUntilFinished) {}
+            public void onTick(long millisUntilFinished) { }
 
             public void onFinish() {
                 final SharedPreferences.Editor editor = prefs.edit();
                 editor.putBoolean("sleep_timer_running", false);
                 editor.apply();
 
-                final Intent intentSleepTimer = new Intent();
-                intentSleepTimer.setAction("sleep_timer");
-                LocalBroadcastManager.getInstance(ctx).sendBroadcast(intentSleepTimer);
+                mMediaBrowser = new MediaBrowserCompat(mContext,
+                        new ComponentName(mContext, MediaPlayerService.class),
+                        connectionCallbacks,
+                        null);
+
+                mMediaBrowser.connect();
+
+                //final Intent intentSleepTimer = new Intent();
+                //intentSleepTimer.setAction("sleep_timer");
+                //LocalBroadcastManager.getInstance(mContext).sendBroadcast(intentSleepTimer);
 
                 stopSelf();
             }
@@ -81,6 +94,25 @@ public class SleepTimerService extends IntentService {
 
         return START_STICKY;
     }
+
+    private final MediaBrowserCompat.ConnectionCallback connectionCallbacks = new MediaBrowserCompat.ConnectionCallback() {
+                @Override
+                public void onConnected() {
+                    try {
+                        final MediaControllerCompat mediaController = new MediaControllerCompat(mContext, mMediaBrowser.getSessionToken());
+                        mediaController.getTransportControls().pause();
+                    } catch (RemoteException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                @Override
+                public void onConnectionSuspended() { }
+
+                @Override
+                public void onConnectionFailed() { }
+            };
+
 
     @Override
     public void onDestroy() {
