b3b2bf6c2fbf91643fa2373c80059612a0d01d75
Kris Bernardic <krisdb@gmail.com>
2020-01-05 18:34:32 -0500

    premium

--- wear/src/main/java/com/krisdb/wearcasts/Utilities/EpisodeUtilities.java
+++ wear/src/main/java/com/krisdb/wearcasts/Utilities/EpisodeUtilities.java
@@ -254,7 +254,7 @@ public class EpisodeUtilities {
         //Log.d(ctx.getPackageName(), "Trimming " + podcast.getPodcastId());
         final SQLiteDatabase sdb = DatabaseHelper.select(ctx);
 
-        final String numberOfEpisode = PreferenceManager.getDefaultSharedPreferences(ctx).getString("pref_episode_limit", ctx.getString(R.string.episode_list_default));
+        final String numberOfEpisode = Utilities.hasPremium(ctx) ? PreferenceManager.getDefaultSharedPreferences(ctx).getString("pref_episode_limit", ctx.getString(R.string.episode_list_default)) : ctx.getString(R.string.episode_list_default);
 
         //sdb.execSQL("DELETE FROM [tbl_podcast_episodes] WHERE [id] IN (SELECT [id] FROM [tbl_podcast_episodes] WHERE [pid] = ? ORDER BY [pubdate] DESC LIMIT 1000 OFFSET ".concat(numberOfEpisode).concat(")"),
         sdb.execSQL("DELETE FROM [tbl_podcast_episodes] WHERE [id] IN (SELECT [id] FROM [tbl_podcast_episodes] WHERE [pid] = ? AND downloadid = 0 AND download = 0 AND position = 0 AND playing = 0 ORDER BY [pubdate] DESC LIMIT 1000 OFFSET ".concat(numberOfEpisode).concat(") AND [id] NOT IN (SELECT pe.id FROM tbl_podcast_episodes AS pe JOIN tbl_playlists_xref AS pex ON pe.id == pex.episode_id WHERE pe.pid = ?)"),
@@ -593,7 +593,7 @@ public class EpisodeUtilities {
 
         final boolean hidePlayed = prefs.getBoolean("pref_" + podcastId + "_hide_played", false);
 
-        final int numberOfEpisode = Integer.valueOf(prefs.getString("pref_episode_limit", ctx.getString(R.string.episode_list_default)));
+        final int numberOfEpisode = Utilities.hasPremium(ctx) ? Integer.valueOf(prefs.getString("pref_episode_limit", ctx.getString(R.string.episode_list_default))) : Integer.valueOf(ctx.getString(R.string.episode_list_default));
 
         return GetEpisodes(ctx, podcastId, hidePlayed, numberOfEpisode, null);
     }
