ca9f8482a3752288f13d470c30d9884fdc9d4a1f
Kris Bernardic <krisdb@gmail.com>
2020-03-04 15:04:00 -0500

    remove import

--- library/src/main/java/com/krisdb/wearcastslibrary/FeedParser.java
+++ library/src/main/java/com/krisdb/wearcastslibrary/FeedParser.java
@@ -6,7 +6,6 @@ import org.xmlpull.v1.XmlPullParserFactory;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 import static com.krisdb.wearcastslibrary.DateUtils.FormatDate;
